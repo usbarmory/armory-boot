864ea18016e29181ef0668b883820e2d324ce72b
Andrej Rosano <andrej.rosano@f-secure.com>
2021-11-19 10:30:40 +0100

    Define default boot parameters

--- disk/disk.go
+++ disk/disk.go
@@ -16,18 +16,31 @@ import (
 	"github.com/f-secure-foundry/tamago/board/f-secure/usbarmory/mark-two"
 )
 
+// DefaultBootDevice is the default boot device
+const DefaultBootDevice = "uSD"
+
+// DefaultOffset is the default start offset of the ext4 partition
+const DefaultOffset = 5242880
+
 // Detect initializes the USB armory internal flash ("eMMC") or external
 // microSD card ("uSD") as boot device, an ext4 partition must be present at
-// the passed start offset.
+// the passed start offset. An empty value for device or start parameter selects
+// its default value.
 func Detect(device string, start string) (part *Partition, err error) {
-	offset, err := strconv.ParseInt(start, 10, 64)
+	offset := int64(DefaultOffset)
 
-	if err != nil {
-		return nil, fmt.Errorf("invalid start offset, %v\n", err)
+	if len(start) > 0 {
+		if offset, err = strconv.ParseInt(start, 10, 64); err != nil {
+			return nil, fmt.Errorf("invalid start offset, %v\n", err)
+		}
 	}
 
 	part = &Partition{
 		Offset: offset,
+	}
+
+	if len(device) == 0 {
+		device = DefaultBootDevice
 	}
 
 	switch device {
