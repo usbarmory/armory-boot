da187922209fb7fe2c090b15102a18a30f571f3f
Andrea Barisani <andrea@inversepath.com>
2022-09-05 12:57:40 +0200

    allow package import from tamago/riscv64

---
+++ exec/boot_riscv64.go
@@ -0,0 +1,12 @@
+// https://github.com/usbarmory/armory-boot
+//
+// Copyright (c) WithSecure Corporation
+// https://foundry.withsecure.com
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+package exec
+
+func boot(kernel uint32, params uint32, cleanup func()) (err error) {
+	panic("unimplemented")
+}
