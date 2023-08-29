9a8225333154b7d17223efb9d916b54a4137acbe
Andrea Barisani <andrea@inversepath.com>
2023-08-29 17:16:57 +0200

    replace magic numbers with explanation of HID Report ID role

--- cmd/armory-boot-usb/armory-boot-usb.go
+++ cmd/armory-boot-usb/armory-boot-usb.go
@@ -37,6 +37,15 @@ const (
 	iramOffset = 0x00910000
 )
 
+// SDP HID report IDs
+// (p327, 8.9.3.1 SDP commands, IMX6ULLRM).
+const (
+	H2D_COMMAND       = 1 // Command  - Host to Device
+	H2D_DATA          = 2 // Data     - Host to Device
+	D2H_RESPONSE      = 3 // Response - Device to Host
+	D2H_RESPONSE_LAST = 4 // Response - Device to Host
+)
+
 // This tool should work with all SoCs from the i.MX series capable of USB HID
 // based SDP, only tested devices are listed as supported, Pull Requests are
 // welcome to expand this set.
@@ -98,10 +107,10 @@ func detect() (err error) {
 	return
 }
 
-func sendHIDReport(n int, buf []byte, wait int) (res []byte, err error) {
-	err = conf.dev.Write(append([]byte{byte(n)}, buf...))
+func sendHIDReport(reqID int, buf []byte, resID int) (res []byte, err error) {
+	err = conf.dev.Write(append([]byte{byte(reqID)}, buf...))
 
-	if err != nil || wait < 0 {
+	if err != nil || resID < 0 {
 		return
 	}
 
@@ -115,7 +124,7 @@ func sendHIDReport(n int, buf []byte, wait int) (res []byte, err error) {
 				return nil, errors.New("error reading response")
 			}
 
-			if len(res) > 0 && res[0] == byte(wait) {
+			if len(res) > 0 && res[0] == byte(resID) {
 				return
 			}
 		case <-timer:
@@ -127,13 +136,13 @@ func sendHIDReport(n int, buf []byte, wait int) (res []byte, err error) {
 func dcdWrite(dcd []byte, addr uint32) (err error) {
 	r1, r2 := sdp.BuildDCDWriteReport(dcd, addr)
 
-	_, err = sendHIDReport(1, r1, -1)
+	_, err = sendHIDReport(H2D_COMMAND, r1, -1)
 
 	if err != nil {
 		return
 	}
 
-	_, err = sendHIDReport(2, r2, 4)
+	_, err = sendHIDReport(H2D_DATA, r2, D2H_RESPONSE)
 
 	return
 }
@@ -141,21 +150,21 @@ func dcdWrite(dcd []byte, addr uint32) (err error) {
 func fileWrite(imx []byte, addr uint32) (err error) {
 	r1, r2 := sdp.BuildFileWriteReport(imx, addr)
 
-	_, err = sendHIDReport(1, r1, -1)
+	_, err = sendHIDReport(H2D_COMMAND, r1, -1)
 
 	if err != nil {
 		return
 	}
 
-	wait := -1
+	resID := -1
 	timer := time.After(time.Duration(conf.timeout) * time.Second)
 
 	for i, r := range r2 {
 		if i == len(r2)-1 {
-			wait = 4
+			resID = D2H_RESPONSE_LAST
 		}
 	send:
-		_, err = sendHIDReport(2, r, wait)
+		_, err = sendHIDReport(H2D_DATA, r, resID)
 
 		if err != nil && runtime.GOOS == "darwin" && err.Error() == "hid: general error" {
 			// On macOS access contention with the OS causes
@@ -172,7 +181,7 @@ func fileWrite(imx []byte, addr uint32) (err error) {
 					DataCount:   uint32(len(imx)) - off,
 				}
 
-				if _, err = sendHIDReport(1, r1.Bytes(), -1); err != nil {
+				if _, err = sendHIDReport(H2D_COMMAND, r1.Bytes(), -1); err != nil {
 					return
 				}
 
@@ -190,7 +199,7 @@ func fileWrite(imx []byte, addr uint32) (err error) {
 
 func jumpAddress(addr uint32) (err error) {
 	r1 := sdp.BuildJumpAddressReport(addr)
-	_, err = sendHIDReport(1, r1, -1)
+	_, err = sendHIDReport(H2D_COMMAND, r1, -1)
 
 	return
 }
